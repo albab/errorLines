010cf67cef9ddea463b55151cb1bc70f30117dcc
zhurrle <zhurrle@asu.edu>
2014-04-06 22:19:09 -0700

    Update main.cpp
    I changed how hubs are printed and put in a call to flightList.search() (the call is broken though)

--- main.cpp
+++ main.cpp
@@ -63,29 +63,18 @@ int main() {
 	do {
 	cout << "\nWould you like to book a flight? (press y for yes or press q to quit)\n";
 	cin>>i;
-	char destination [50];
+	char destination[100];
 	char date [10];
 
 	switch(i) {
 	case 'y':
-		cout<<"\nWhere would you like to fly? [Enter a number]\n\n";
-		cout<<"1: Albany [Albany International Airport]\n";
-		cout<<"2: Boston [General Edward Lawrence Logan International Airport]\n";
-		cout<<"3: Chicago [Midway International Airport]\n";
-		cout<<"4: Chicago [O'Hare International Airport]\n";
-		cout<<"5: Denver [Denver International Airport]\n";
-		cout<<"6: Honolulu [Honolulu International Airport]\n";
-		cout<<"7: Las Vegas [Mc Carran International Airport]\n";
-		cout<<"8: Los Angeles [Los Angeles International Airport]\n";
-		cout<<"9: Los Angeles [Whiteman Airport]\n";
-		cout<<"10: Miami [Miami International Airport]\n";
-		cout<<"11: New York [John F Kennedy International Airport]\n";
-		cout<<"12: Phoenix [Phoenix Sky Harbor International Airport]\n";
-		cout<<"13: San Diego [San Diego International Airport]\n";
-		cout<<"14: San Francisco [San Francisco International Airport]\n";
-		cout<<"15: Tucson [Tucson International Airport]\n\n";
-		cout<<"#";
+		cout<<"\nWhere would you like to fly? [Enter airport name exactly]\n\n";
+		hubList.Print();
+		cout<<"Destination: ";
+
 		cin>>destination;		//user's input to where the want to fly to
+
+		flightList.search(destination);
 
 		cout<<"\nWould you like the cheapest flight or the shortest flight\n (press c for cheapest or press s for shortest)\n";
 		filter='f';
@@ -96,7 +85,7 @@ int main() {
 			switch (filter)
 			{
 				case 'c':
-					///calculate the cheapest flight
+					//calculate the cheapest flight
 					break;
 				case 's':
 					//calculate the shortest flight
@@ -149,15 +138,3 @@ int main() {
 
 return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
